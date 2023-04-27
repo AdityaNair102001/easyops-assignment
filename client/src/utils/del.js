9b1816b8e0d4768258f9eb20b36741451e470572
AdityaNair102001 <72807512+AdityaNair102001@users.noreply.github.com>
2023-04-27 12:45:43 +0530

    feat: persistence added to delete feature

--- client/src/utils/del.js
+++ client/src/utils/del.js
@@ -1,3 +1,6 @@
 export default function del(setTable, index) {
   setTable((table) => table.filter((row, i) => i !== index));
+  const oldData = JSON.parse(localStorage.getItem("easyops"));
+  const newData = oldData.filter((row, i) => i !== index);
+  localStorage.setItem("easyops", JSON.stringify(newData));
 }
